2e5b2d4a66022fd2618997fe71d8554914255b52
mbeuil <mbeuil@student.42.fr>
2020-02-10 10:39:30 +0100

    add eslint and prettier with airbnb config

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
-import Header from "./components/header/header.component";
-import Footer from "./components/footer/footer.component";
-import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import Footer from './components/footer/footer.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
-import "./App.css";
+import './App.css';
 
 class App extends Component {
   constructor(props) {
