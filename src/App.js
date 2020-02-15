26a887b06eb5cc649fe2cebfde417a55afd1878d
mbeuil <mbeuil@student.42.fr>
2020-02-15 18:09:15 +0100

    add cone shadow to the cartDropDown component

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ const App = () => {
     return () => {
       unsubscribe();
     };
-  }, []);
+  }, [dispatch]);
 
   const handleRedirection = () => {
     return currentUser ? <Redirect to="/" /> : <SignInAndRegisterPage />;
