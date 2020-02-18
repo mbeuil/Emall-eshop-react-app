d0e4318bd84736a66f0523e6ecc2dd8875a7dc6c
mbeuil <mbeuil@student.42.fr>
2020-02-18 14:37:26 +0100

    add collection page component, .jsx and .css. switch to grid display for collection preview. Add a collection id map to shop reducer and selectshopcollection selector to render individual collection based on collectionId from match param

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+import { selectShopCollection } from '../../redux/shop/shop.selectors';
+
+import './collection.style.css';
+
+const CollectionPage = ({ match }) => {
+  const { title, items } = useSelector(selectShopCollection(match.params.collectionId));
+
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CollectionPage;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionOverview from '../../components/collection-overview/collection-overview.component';
+import CollectionPage from '../collection/collection.component';
 
 import './shop.style.css';
 
-const ShopPage = () => {
+const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
-      <CollectionOverview />
+      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   );
 };
