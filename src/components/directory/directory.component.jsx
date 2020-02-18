3a9d6b778bf8fe4e64795d4c19b1cea233299bf0
mbeuil <mbeuil@student.42.fr>
2020-02-18 10:54:56 +0100

    add directoy state to redux, create a sections selector, change directory component form class to function + add reduc hook for comp's state

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,62 +1,21 @@
-import React, { Component, memo } from 'react';
+import React, { memo } from 'react';
+import { useSelector } from 'react-redux';
 
 import MenuItem from '../menu-item/menu-item.component';
+import { selecDirectorySections } from '../../redux/directory/directory.selectors';
 
 import './directory.style.css';
 
-class Directory extends Component {
-  constructor(props) {
-    super(props);
+const Directory = () => {
+  const sections = useSelector(selecDirectorySections);
 
-    this.state = {
-      sections: [
-        {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1,
-          linkUrl: 'hats'
-        },
-        {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2,
-          linkUrl: 'shop/jackets'
-        },
-        {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3,
-          linkUrl: 'shop/sneakers'
-        },
-        {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
-          id: 4,
-          linkUrl: 'shop/womens'
-        },
-        {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          size: 'large',
-          id: 5,
-          linkUrl: 'shop/mens'
-        }
-      ]
-    };
-  }
-
-  render() {
-    const { sections } = this.state;
-
-    return (
-      <div className="directory-menu">
-        {sections.map(({ id, ...otherSectionProps }) => (
-          <MenuItem key={id} {...otherSectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="directory-menu">
+      {sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={id} {...otherSectionProps} />
+      ))}
+    </div>
+  );
+};
 
 export default memo(Directory);
