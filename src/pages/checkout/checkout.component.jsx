e90ca09fabead0620b460ef1d488f5e625653b30
mbeuil <mbeuil@student.42.fr>
2020-02-19 10:14:01 +0100

    small style change

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -35,12 +35,12 @@ const CheckoutPage = () => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <div className="total">{total}</div>
+      <StripeCheckoutButton className="stripe" price={cartTotal} />
       <div className="test-warning">
         *Please use the following test credit card for payments*
         <br />
         4242 4242 4242 4242 - Exp: 02/20 - CVV: 123
       </div>
-      <StripeCheckoutButton price={cartTotal} />
     </div>
   );
 };
