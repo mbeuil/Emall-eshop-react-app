f3fba3a9824bb538019c418ac44b751ea80a658a
mbeuil <mbeuil@student.42.fr>
2020-02-25 17:14:44 +0100

    add route to collectionPreview, router's hooks were used

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,15 +1,32 @@
 /** @format */
 
 import React, { memo } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import CollectionItem from '../collection-item/collection-item.component';
 
 import * as S from './collection-preview.styles';
 
-const CollectionPreview = ({ title, items }) => {
+const CollectionPreview = ({ title, items, routeName }) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const previewToShopPage = () => {
+    history.push(`${location.pathname}/${routeName}`);
+  };
+
   return (
     <S.CollectionPreviewContainer>
-      <S.CollectionTitle>{title.toUpperCase()}</S.CollectionTitle>
+      <S.CollectionTitle>
+        <S.TitleContainer
+          onClick={previewToShopPage}
+          onKeyDown={previewToShopPage}
+          role="button"
+          tabIndex={0}
+        >
+          {title.toUpperCase()}
+        </S.TitleContainer>
+      </S.CollectionTitle>
       <S.CollectionItems>
         {items
           .filter((item, index) => index < 4)
--- src/components/collection-preview/collection-preview.styles.js
+++ src/components/collection-preview/collection-preview.styles.js
@@ -8,8 +8,32 @@ export const CollectionPreviewContainer = styled.div`
 `;
 
 export const CollectionTitle = styled.h1`
+  position: relative;
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+
+  &:before {
+    border-top: 1px solid grey;
+    content: '';
+    position: absolute;
+    top: 50%;
+    width: 100%;
+    z-index: -1;
+  }
+`;
+
+export const TitleContainer = styled.span`
   font-size: 28px;
-  margin-bottom: 25px;
+  background-color: white;
+  padding-right: 10px;
+  outline: none;
+  color: grey;
+  cursor: pointer;
+
+  &:hover {
+    color: black;
+  }
 `;
 
 export const CollectionItems = styled.div`
