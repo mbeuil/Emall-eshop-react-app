21d127994ce3b3f7fa3c18a2a3c781deffea953e
mbeuil <mbeuil@student.42.fr>
2020-09-23 16:23:02 +0200

    create /payment server's route

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = requre("body-parser");
-const path = require("path");
+/** @format */
 
-if (process.env.NODE_ENV !== "production") require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = requre('body-parser');
+const path = require('path');
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,15 +17,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "cliebt/build", "index.html"));
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'cliebt/build', 'index.html'));
   });
 }
 
 app.listen(port, (error) => {
   if (error) throw error;
-  console.log("server running on port" + port);
+  console.log('server running on port' + port);
+});
+
+app.post('/payment', (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd',
+  };
+
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ succes: stripeRes });
+    }
+  });
 });
