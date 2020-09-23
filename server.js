a0e20bea195ceef5ef2eb5d4c66a73e015600d84
mbeuil <mbeuil@student.42.fr>
2020-09-23 21:36:15 +0200

    server.js error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const cors = require('cors');
-const bodyParser = requre('body-parser');
+const bodyParser = require('body-parser');
 const path = require('path');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
