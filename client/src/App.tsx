13eeb7c634f1b2f39a5111a3fb4e1f7eb43d0617
mbeuil <mbeuil@student.42.fr>
2020-09-28 20:41:14 +0200

    add react lazy and suspense to code split for optimization in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,25 @@
 /** @format */
 
 // node_modules
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
 // Local modules
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import SignInAndRegisterPage from './pages/sign-in-and-register/sign-in-and-register.component';
-import CheckoutPage from './pages/checkout/checkout.component';
-
 import Header from './components/header/header.component';
 import Footer from './components/footer/footer.component';
+import LoadingSpinner from './components/loading-spinner/loading-spinner.component';
 
 // Redux dispatch + selector
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
+
+const HomePage = lazy(() => import('./pages/homepage/homepage.component'));
+const ShopPage = lazy(() => import('./pages/shop/shop.component'));
+const SignInAndRegisterPage = lazy(() =>
+  import('./pages/sign-in-and-register/sign-in-and-register.component'),
+);
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component'));
 
 const App = () => {
   const currentUser = useSelector(selectCurrentUser);
@@ -34,10 +37,12 @@ const App = () => {
     <div>
       <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route exact path="/checkout" component={CheckoutPage} />
-        <Route exact path="/signin" render={handleRedirection} />
+        <Suspense fallback={<LoadingSpinner />}>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
+          <Route exact path="/signin" render={handleRedirection} />
+        </Suspense>
       </Switch>
       <Footer />
     </div>
