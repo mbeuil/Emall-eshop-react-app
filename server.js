921715a172c9594ed1bd8f05f5c893fd2fd00076
mbeuil <mbeuil@student.42.fr>
2020-10-31 22:16:10 +0100

    adding compression library for gzipping on heroku

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const compression = require('compression');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -12,8 +13,9 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compression());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(cors());
 
@@ -25,7 +27,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-app.listen(port, (error) => {
+app.listen(port, error => {
   if (error) throw error;
   console.log('server running on port' + port);
 });
@@ -39,9 +41,9 @@ app.post('/payment', (req, res) => {
 
   stripe.charges.create(body, (stripeErr, stripeRes) => {
     if (stripeErr) {
-      res.status(500).send({ error: stripeErr });
+      res.status(500).send({error: stripeErr});
     } else {
-      res.status(200).send({ succes: stripeRes });
+      res.status(200).send({succes: stripeRes});
     }
   });
 });
